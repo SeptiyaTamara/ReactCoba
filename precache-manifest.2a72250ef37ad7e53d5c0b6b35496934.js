38500d1635c5d6216c9eea4d91a293cf9d48e56c
septiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-04-11 04:27:23 +0700

    Updates

---
+++ precache-manifest.2a72250ef37ad7e53d5c0b6b35496934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SeptiyaTamara/ReactCoba/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "17e670a2f49e9106259b",
+    "url": "/SeptiyaTamara/ReactCoba/static/js/runtime~main.88e32ff3.js"
+  },
+  {
+    "revision": "f3a01712a59e2ef7565c",
+    "url": "/SeptiyaTamara/ReactCoba/static/js/main.3a8530b1.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/SeptiyaTamara/ReactCoba/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f3a01712a59e2ef7565c",
+    "url": "/SeptiyaTamara/ReactCoba/static/css/main.6c0300bd.chunk.css"
+  },
+  {
+    "revision": "afeda18b91542a01d5a69f1989db2118",
+    "url": "/SeptiyaTamara/ReactCoba/index.html"
+  }
+];
